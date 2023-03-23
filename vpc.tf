5ab4b83d183f7e8b09f6f244ea076466778fd5a5
Rahul031011 <124134640+Rahul031011@users.noreply.github.com>
2023-03-23 18:14:37 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -55,7 +55,7 @@ resource "aws_internet_gateway" "first_ig" {
 }
 
 resource "aws_instance" "connect" {
-  ami           = "ami-060d3509162bcc386"
+  ami           = "ami-06604eb73be76c003"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.sub1.id
   key_name   = "california"
